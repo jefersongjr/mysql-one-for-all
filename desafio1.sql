e596151af5ac161e758c5a8b5d066b5c76abfe60
jefersongjr <jeferson_gjr@hotmail.com>
2022-08-17 16:30:50 -0300

    feat : preechendo tabela tracks

--- desafio1.sql
+++ desafio1.sql
@@ -10,13 +10,13 @@ DROP DATABASE IF EXISTS SpotifyClone;
   CREATE TABLE SpotifyClone.plan(
     plan_id INT PRIMARY KEY AUTO_INCREMENT,
     plan_name VARCHAR(20) NOT NULL,
-    plan_price DECIMAL(0, 2) NOT NULL
+    plan_price DECIMAL(9, 2) NOT NULL
   ) engine = InnoDB;
 
   CREATE TABLE SpotifyClone.album(
     album_id INT PRIMARY KEY AUTO_INCREMENT,
     album_name VARCHAR(45) NOT NULL,
-    release_date DATE NOT NULL,
+    release_date INT NOT NULL,
     artist_id INT NOT NULL,
     FOREIGN KEY (artist_id) REFERENCES artist (artist_id) 
   ) engine = InnoDB;
@@ -34,7 +34,6 @@ DROP DATABASE IF EXISTS SpotifyClone;
   CREATE TABLE SpotifyClone.users(
     user_id INT PRIMARY KEY AUTO_INCREMENT,
     user_name VARCHAR(45) NOT NULL,
-    user_last_name VARCHAR(45),
     age INT ,
     plan_id INT NOT NULL,
     subscribe_date DATE NOT NULL,
@@ -71,3 +70,44 @@ DROP DATABASE IF EXISTS SpotifyClone;
     ('familiar', 7.99),
     ('universitario', 5.99),
     ('pessoal', 6.99);
+
+  INSERT INTO SpotifyClone.album (album_name, release_date, artist_id)
+  VALUES
+    ('Renaissance', 2022 , 1),
+    ('Jazz', 1978, 2),
+    ('Hot Space', 1982, 2),
+    ('Falso Brilhante', 1998, 3),
+    ('Vento de Maio', 2001, 3),
+    ('QVVJFA?', 2003, 4),
+    ('Somewhere Far Beyond', 2007, 5),
+    ('I Put A Spell On You', 2013, 6);
+
+  INSERT INTO SpotifyClone.tracks (track_name, duration, artist_id, album_id)
+  VALUES
+    ('BREAK MY SOUL', 279, 1, 1),
+    ('VIRGO’S GROOVE', 369, 1, 1),
+    ('ALIEN SUPERSTAR', 116, 1, 1),
+    ('Don’t Stop Me Now', 203, 2, 1),
+    ('Under Pressure', 152, 2, 2),
+    ('Como Nossos Pais', 105, 3, 1),
+    ('O Medo de Amar é o Medo de Ser Livre', 207, 3, 2),
+    ('Samba em Paris', 267, 4, 1),
+    ('The Bard’s Song', 244, 5, 1),
+    ('I Put A Spell On You', 100, 6, 1);
+
+    INSERT INTO SpotifyClone.users (user_name, age, artist_id, plan_id,  subscribe_date)
+  VALUES
+    ('Barbara Liskov', 2022 , 1),
+    ('Robert Cecil Martin', 1978, 2),
+    ('Ada Lovelace', 1982, 2),
+    ('Martin Fowler', 1998, 3),
+    ('Sandi Metz', 2001, 3),
+    ('Paulo Freire', 2003, 4),
+    ('Bell Hooks', 2007, 5),
+    ('Christopher Alexander', 2003, 4),
+    ('Somewhere Far Beyond', 2007, 5),
+    ('QVVJFA?', 2003, 4),
+    ('Somewhere Far Beyond', 2007, 5),
+    ('QVVJFA?', 2003, 4),
+    ('Somewhere Far Beyond', 2007, 5),
+    ('I Put A Spell On You', 2013, 6);
