cfc620607aea3ae34ab8501c91a70b142ef26bba
jefersongjr <jeferson_gjr@hotmail.com>
2022-08-20 22:01:44 -0300

    feat: requisito 10 - criando query para filtrar musicas reproduzidas no plano gratuito

---
+++ desafio10.sql
@@ -0,0 +1,10 @@
+SELECT 
+    track_name AS nome, 
+    count(track_name) AS reproducoes
+FROM SpotifyClone.history as history
+    INNER JOIN SpotifyClone.users AS users ON history.user_id = users.user_id
+    INNER JOIN SpotifyClone.tracks AS tracks ON history.track_id = tracks.track_id
+    INNER JOIN SpotifyClone.plan AS plan ON plan.plan_id = users.plan_id
+where plan_name = 'gratuito' OR plan_name = 'pessoal'
+GROUP BY track_name
+ORDER BY track_name;
